6ee723353917be855c5fae7e61af683cf3b688b6
ohalewe chidiebere <105126409+The5thlewe@users.noreply.github.com>
2023-02-19 21:27:57 +0500

    Add files via upload

---
+++ components/profiles.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function profiles({ Leaderboard }) {
+  return <div id="profile">{Item(Leaderboard)}</div>;
+}
+
+function Item(data) {
+  return (
+    <>
+      {data.map((value, index) => (
+        <div className="flex" key={index}>
+          <div className="item">
+            <img src={value.img} alt=""></img>
+
+            <div className="info">
+              <h3 className="text name-dark">{value.name}</h3>
+              <span>{value.location}</span>
+            </div>
+          </div>
+          <div className="item">
+            <span>{value.score}</span>
+          </div>
+        </div>
+      ))}
+      ;
+    </>
+  );
+}
